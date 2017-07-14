e2ba8efc3f99e423931dd50a89ba15d3c4bf12e9
harrytwright <haroldtomwright@gmail.com>
2017-07-14 16:08:41 +0100

    fixed untracked files

--- Example/Pods/Target Support Files/Ardex/Ardex-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Ardex : NSObject
-@end
-@implementation PodsDummy_Ardex
-@end
--- Example/Pods/Target Support Files/Pods-Ardex_Example/Pods-Ardex_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ardex_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Ardex_Example
-@end
